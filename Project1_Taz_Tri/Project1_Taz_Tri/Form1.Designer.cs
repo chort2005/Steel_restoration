086840e601bbc6b8835dc1bb3e5b7171b87533ed
chort2005 <124133043+chort2005@users.noreply.github.com>
2024-02-15 18:36:38 +0500

    Project_1.1
    Версия 1.1 Beta
    В этой версий был изменен дизайн приложения.

--- Project1_Taz_Tri/Project1_Taz_Tri/Form1.Designer.cs
+++ Project1_Taz_Tri/Project1_Taz_Tri/Form1.Designer.cs
@@ -34,19 +34,23 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 62);
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(35, 79);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 43);
+            this.button1.Size = new System.Drawing.Size(270, 61);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Конопка";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(35, 21);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(270, 20);
+            this.textBox1.Size = new System.Drawing.Size(270, 52);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -54,6 +58,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
             this.ClientSize = new System.Drawing.Size(333, 152);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
